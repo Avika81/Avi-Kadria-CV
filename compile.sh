cc3c3612d7c9d224d93e8de7b986d5e7485c73a6
Avi Kadria <avi.kadria3@gmail.com>
2026-01-18 12:15:32 +0200

    added some nice commands to compile.sh

--- compile.sh
+++ compile.sh
@@ -2,3 +2,9 @@ xelatex resume.tex
 
 # reqs:
 # sudo apt install texlive-bibtex-extra biber texlive-fontsextra
+
+# clean stuff:
+# rm -f *.bcf *.aux *.log *.run.xml
+
+# regenerate bibfile:
+# biber resume
